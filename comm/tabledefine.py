e302510db548acef26b042c9eed1162f6c679067
whg <whg@taolesoft.com>
2016-07-19 15:29:14 +0800

    加入表名类

---
+++ comm/tabledefine.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+#!/usr/bin/python
+class TableNameS(object):
+	suibo_user_info='suibo_user_info'
+	suibo_user_action='suibo_user_action'
+	hongbao_shareuin_info ='hongbao_shareuin_info'
+	suibo_events = 'suibo_events'
+	suibo_room_sendgift_info = 'suibo_room_sendgift_info'
+	suibo_room_liaot_info = 'suibo_room_liaot_info'
+	suibo_room_login_info = 'suibo_room_login_info'
+	suibo_usr_logout= 'suibo_usr_logout'
+	suibo_usr_closevideo = 'suibo_usr_closevideo'
